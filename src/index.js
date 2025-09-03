aed2ac6fa92e105c7beabb9323a585d3604efa74
NN125 <nicolas.nolden@student.swic.edu>
2025-09-03 18:35:28 -0500

    Create calculateLetterGrade

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const GRADE_A_THRESHOLD = 90;
 const GRADE_B_THRESHOLD = 80;
 const GRADE_C_THRESHOLD = 70;
 const GRADE_D_THRESHOLD = 60;
+
+const calculateLetterGrade = (numericGrade) => {
+  // Add your grade calculation logic here
+};
