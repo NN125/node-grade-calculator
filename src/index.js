7eb400f686d5ab1771457a43d7ad22e04f8b3008
NN125 <nicolas.nolden@student.swic.edu>
2025-09-05 02:00:59 -0500

    Cleanup calculateLetterGrade

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ const GRADE_D_THRESHOLD = 60;
 
 const calculateLetterGrade = (numericGrade) => {
   if (numericGrade >= GRADE_A_THRESHOLD) {
-    return console.log("Grade " + numericGrade + ": A");
-}
-  else if (numericGrade >= GRADE_B_THRESHOLD) {
-    return console.log("Grade " + numericGrade + ": B");
+    return console.log(`Grade ${numericGrade}: A`);
+  } else if (numericGrade >= GRADE_B_THRESHOLD) {
+    return console.log(`Grade ${numericGrade}: B`);
+  } else if (numericGrade >= GRADE_C_THRESHOLD) {
+    return console.log(`Grade ${numericGrade}: C`);
+  } else if (numericGrade >= GRADE_D_THRESHOLD) {
+    return console.log(`Grade ${numericGrade}: D`);
   }
-  else if (numericGrade >= GRADE_C_THRESHOLD) {
-    return console.log("Grade " + numericGrade + ": C");
-  }
-  else if (numericGrade >= GRADE_D_THRESHOLD) {
-    return console.log("Grade " + numericGrade + ": D");
-  } else {
-    return console.log("Grade " + numericGrade + ": F");
-  }
+  return console.log(`Grade ${numericGrade}: F`);
+};
